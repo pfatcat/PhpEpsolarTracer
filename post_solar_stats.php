eb827fd2b0bcabf00e6674d817ea78325912365f
Chuck Pfaff <chuck@fortyau.com>
2020-12-21 16:50:15 -0600

    maybe

--- post_solar_stats.php
+++ post_solar_stats.php
@@ -106,7 +106,9 @@ if ($tracer->getRealtimeData()) {
 	print "\nRealTime Data\n";
 	print "----------------------------------\n";
 
-	echo implode(" ",$tracer->realtimeData);
+	echo implode(" ",$tracer->realtimeData) . "\n";
+	print "voltage: " . $tracer->realtimeData[3] ."\n";
+	
 	for ($i = 0; $i < count($tracer->realtimeData); $i++)
 		print str_pad($i, 2, '0', STR_PAD_LEFT)." ".$tracer->realtimeKey[$i].": ".$tracer->realtimeData[$i].$tracer->realtimeSym[$i]."\n";
 		#post_to_firebase(44);
